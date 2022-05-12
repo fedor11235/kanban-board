42c7aec891044c69a100e872958969acd9337fd8
Fedor <fedoravdeev3@gmail.com>
2022-05-12 20:36:42 +0200

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,4 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'kanban-board',
